564ee6a1f8f9d8ea5ce3407bca07867b02d6e745
kss <kss53043@naver.com>
2024-10-30 20:19:24 +0900

    로그인, 회원가입 페이지 작성

--- sprint-m1/script.js
+++ sprint-m1/script.js
@@ -6,3 +6,16 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scr');
     }
 });
+
+const pwViewBtn = document.querySelectorAll('.form_inp_list > li > .desc > .btn');
+pwViewBtn.forEach(function(e) {
+    e.addEventListener('click', function() {
+        e.classList.toggle('active');
+        const inp = e.closest('li').querySelector('input');
+        if (inp.getAttribute('type') === 'password') {
+            inp.setAttribute('type', 'text');
+        } else {
+            inp.setAttribute('type', 'password');
+        }
+    });
+});
