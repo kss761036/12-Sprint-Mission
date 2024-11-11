831ce86b2d9c9a1f47988c4253fea528e8e1b230
김승석 <kss@gimseungseog-ui-MacBookAir.local>
2024-11-12 02:02:52 +0900

    스크립트 변경

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-window.addEventListener('scroll', function() {
+// 헤더 스크롤 시 그림자
+const hdScroll = function(){
     const header = document.querySelector('.header');
     if (window.scrollY > 5) {
         header.classList.add('scr');
     } else {
         header.classList.remove('scr');
     }
-});
+};
+window.addEventListener('scroll', hdScroll);
 
+// 로그인 회원가입 페이지 비밀번호 토글버튼
+const pwViewToggle = function(e){
+    e.classList.toggle('active');
+    const inp = e.closest('li').querySelector('input');
+    if (inp.getAttribute('type') === 'password') {
+        inp.setAttribute('type', 'text');
+    } else {
+        inp.setAttribute('type', 'password');
+    }
+};
 const pwViewBtn = document.querySelectorAll('.form_inp_list > li > .desc > .btn');
 pwViewBtn.forEach(function(e) {
     e.addEventListener('click', function() {
-        e.classList.toggle('active');
-        const inp = e.closest('li').querySelector('input');
-        if (inp.getAttribute('type') === 'password') {
-            inp.setAttribute('type', 'text');
-        } else {
-            inp.setAttribute('type', 'password');
-        }
+        pwViewToggle(e);
     });
 });
+
+//추가
