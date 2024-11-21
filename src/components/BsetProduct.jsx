5b959b0db9d44be26a6edeba4fa325565f413426
김승석 <kss@gimseungseog-ui-MacBookAir.local>
2024-11-22 00:38:41 +0900

    html css 작성

---
+++ src/components/BsetProduct.jsx
@@ -0,0 +1,14 @@
+import ProductItem from './ProductItem.jsx';
+
+const BsetProduct = () => {
+    return(
+        <ul className='product_list col4'>
+            <li><ProductItem /></li>
+            <li><ProductItem /></li>
+            <li><ProductItem /></li>
+            <li><ProductItem /></li>
+        </ul>
+    )
+}
+
+export default BsetProduct;
