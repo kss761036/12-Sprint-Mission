3975e6c3742e9f2d2242aa4b69cad12588f6dfd7
kss <kss53043@naver.com>
2024-11-23 17:40:47 +0900

    sprint mission5

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,41 +1,26 @@
-import React, { useState, useEffect } from 'react';
 import './ProductItem.css';
-import item from "./../assets/item.png";
-import wish from "./../assets/icon_wish.svg";
+import default_Img from "./../assets/item.png";
+import wishImg from "./../assets/icon_wish.svg";
 
-const ProductItem = () => {
-
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-		const fetchData = async() => {
-            const res = await fetch('https://panda-market-api.vercel.app/docs/');
-                const result = res.json();
-                return result;
-        }	
-        
-        fetchData().then(res => setData(res));
-    }, []);
-
-    console.log(data);
-    
+const ProductItem = ({item}) => {
+    const {images, name, price, favoriteCount} = item;
     return(
         <div className="product_item">
             <div className="thum">
                 <a href="">
-                    <img src={item} alt="아이템 제목" />
+                    <img src={images && images[0] ? images[0] : default_Img} alt={name} onError={(e) => e.target.src = default_Img}/>
                 </a>
             </div>
             <div className="content">
                 <div className="title">
-                    <a href="">아이패드 미니 팝니다.</a>
+                    <a href="">{name}</a>
                 </div>
-                <div className="price">500,000원</div>
+                <div className="price">{price.toLocaleString('ko-KR')}원</div>
                 <div className="wish">
                     <div className="icon">
-                        <img src={wish} alt="찜하기" />
+                        <img src={wishImg} alt="찜하기" />
                     </div>
-                    <div className="num">240</div>
+                    <div className="num">{favoriteCount}</div>
                 </div>
             </div>
         </div>
