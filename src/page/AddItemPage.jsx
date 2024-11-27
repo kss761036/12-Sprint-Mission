14236dae1ec384c3c8327938d291badd28691fa4
kss <kss53043@naver.com>
2024-11-27 15:30:52 +0900

    input 제어

--- src/page/AddItemPage.jsx
+++ src/page/AddItemPage.jsx
@@ -1,16 +1,66 @@
 import SubTitle from "./../components/SubTitle";
 import "./AddItemPage.css";
 import removeIcon from "./../assets/icon_remove.svg";
+import FileInput from "./../components/FileInput";
+import { useState } from "react";
 
 const AddItemPage = () => {
+  const [inputValue, setInputValue] = useState({
+    name: "",
+    introduce: "",
+    price: "",
+  });
+
+  // const [name, setName] = useState("");
+  // const [introduce, seIntroduce] = useState("");
+  // const [price, setPrice] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "price") {
+      const priceValue = value.replaceAll(",", "");
+      const nextPrice = Number(priceValue) || 0;
+      setInputValue((prev) => ({
+        ...prev,
+        [name]: nextPrice.toLocaleString(),
+      }));
+    } else {
+      setInputValue((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleFileChange = (e) => {};
+
+  // const handleNameChange = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const handleIntroduceChange = (e) => {
+  //   seIntroduce(e.target.value);
+  // };
+  // const handlePriceChange = (e) => {
+  //   const priceValue = e.target.value.replaceAll(",", "");
+  //   const nextPrice = Number(priceValue) || 0;
+  //   setPrice(nextPrice.toLocaleString());
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputValue);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <main className="main">
         <div className="inner">
           <div className="tit_box">
             <SubTitle name="상품 등록하기" />
             <div className="add_submit">
-              <button className="btn_blue sml">등록</button>
+              <button type="submit" className="btn_blue sml">
+                등록
+              </button>
             </div>
           </div>
           <div className="add_form">
@@ -20,7 +70,7 @@ const AddItemPage = () => {
                   상품이미지
                 </label>
                 <div className="input_box">
-                  <input type="file" id="add_img" />
+                  <FileInput name="img" onChange={handleFileChange} />
                 </div>
               </li>
               <li>
@@ -28,7 +78,7 @@ const AddItemPage = () => {
                   상품명
                 </label>
                 <div className="input_box">
-                  <input type="text" id="add_name" className="inp_reset" placeholder="상품명을 입력해주세요" />
+                  <input type="text" id="add_name" name="name" className="inp_reset" value={inputValue.name} onChange={handleChange} placeholder="상품명을 입력해주세요" />
                 </div>
               </li>
               <li>
@@ -36,7 +86,7 @@ const AddItemPage = () => {
                   상품 소개
                 </label>
                 <div className="input_box">
-                  <textarea name="" id="add_introduce" className="inp_reset" placeholder="상품 소개를 입력해주세요"></textarea>
+                  <textarea id="add_introduce" name="introduce" className="inp_reset" value={inputValue.introduce} onChange={handleChange} placeholder="상품 소개를 입력해주세요"></textarea>
                 </div>
               </li>
               <li>
@@ -44,7 +94,7 @@ const AddItemPage = () => {
                   판매가격
                 </label>
                 <div className="input_box">
-                  <input type="text" id="add_price" className="inp_reset" placeholder="판매 가격을 입력해주세요" />
+                  <input type="text" id="add_price" name="price" className="inp_reset" value={inputValue.price} onChange={handleChange} placeholder="판매 가격을 입력해주세요" />
                 </div>
               </li>
               <li>
@@ -63,7 +113,7 @@ const AddItemPage = () => {
                   </li>
                   <li>
                     <span>#상의</span>
-                    <button className="btn_reset remove">
+                    <button type="button" className="btn_reset remove">
                       <img src={removeIcon} alt="상의 삭제하기" />
                     </button>
                   </li>
